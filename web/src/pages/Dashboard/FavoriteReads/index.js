75d26c6a8723cb3810e302751faef05290248273
Flávio Arêas <vflavio9@gmail.com>
2022-02-19 17:35:24 -0300

    feat: add reading and favorite books filter

--- web/src/pages/Dashboard/FavoriteReads/index.js
+++ web/src/pages/Dashboard/FavoriteReads/index.js
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import List from '../../../components/List';
 
 import { Container } from '../styles';
 
-function FavoriteReads({ books }) {
+function FavoriteBooks({ books, genres }) {
+  const [filteredBooks, setFilteredBooks] = useState([]);
+  const [currentGenre, setCurrentGenre] = useState('All');
+
+  useEffect(() => {
+    if (currentGenre === 'All') {
+      setFilteredBooks(books);
+      return;
+    }
+
+    const filtered = books.filter((book) => book.genre.includes(currentGenre));
+    setFilteredBooks(filtered);
+  }, [books, currentGenre]);
+
+  async function handleFilterByGenre(genre) {
+    setCurrentGenre(genre);
+  }
+
   return (
     <Container>
-      <h1>Favorite Reads</h1>
+      <h1>Favorite Books</h1>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre}>
+            <button
+              className={currentGenre === genre ? '' : 'outline'}
+              type="button"
+              onClick={() => handleFilterByGenre(genre)}
+            >
+              {genre}
+            </button>
+          </li>
+        ))}
+      </ul>
 
-      <List data={books} />
+      <List data={filteredBooks} />
     </Container>
   );
 }
 
-FavoriteReads.propTypes = {
+FavoriteBooks.propTypes = {
   books: PropTypes.arrayOf.isRequired,
+  genres: PropTypes.arrayOf.isRequired,
 };
 
-export default FavoriteReads;
+export default FavoriteBooks;
--- web/src/pages/Dashboard/Reading/index.js
+++ web/src/pages/Dashboard/Reading/index.js
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import List from '../../../components/List';
 
 import { Container } from '../styles';
 
-function Reading({ books }) {
+function Reading({ books, genres }) {
+  const [filteredBooks, setFilteredBooks] = useState([]);
+  const [currentGenre, setCurrentGenre] = useState('All');
+
+  useEffect(() => {
+    if (currentGenre === 'All') {
+      setFilteredBooks(books);
+      return;
+    }
+
+    const filtered = books.filter((book) => book.genre.includes(currentGenre));
+    setFilteredBooks(filtered);
+  }, [books, currentGenre]);
+
+  async function handleFilterByGenre(genre) {
+    setCurrentGenre(genre);
+  }
+
   return (
     <Container>
       <h1>Reading</h1>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre}>
+            <button
+              className={currentGenre === genre ? '' : 'outline'}
+              type="button"
+              onClick={() => handleFilterByGenre(genre)}
+            >
+              {genre}
+            </button>
+          </li>
+        ))}
+      </ul>
 
-      <List data={books} />
+      <List data={filteredBooks} />
     </Container>
   );
 }
 
 Reading.propTypes = {
   books: PropTypes.arrayOf.isRequired,
+  genres: PropTypes.arrayOf.isRequired,
 };
 
 export default Reading;
